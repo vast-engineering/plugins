c1d6ae75fa13554f0967db62389ed9c1ee6d4ad1
Eric Capps <eric@vast.com>
2012-09-06 11:13:32 -0700

    Add browserify plugin

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ module.exports = {
 	Json: require('./lib/json'),
 	Proxy: require('./lib/proxy'),
 	Error: require('./lib/error'),
-    Static: require('./lib/static')
+    Static: require('./lib/static'),
+    Browserify: require('./lib/browserify')
 };
