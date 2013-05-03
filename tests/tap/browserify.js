0660a248a36b1b2cf307dfb9fcb1de2cd9ce49ed
Tommy Messbauer <tommy@vast.com>
2013-05-03 17:55:55 -0500

    browserify updgraded. need to work out a way to ignore better

--- tests/tap/browserify.js
+++ tests/tap/browserify.js
@@ -1,21 +1,63 @@
 var broadway = require('broadway'),
-    app = { plugins: new broadway.App() },
     tap = require('tap'),
     test = tap.test,
     Browserify = require('../../index').Browserify;
 
-app.plugins.use(new Browserify(), {
-    app: app,
-    compress: true
+test('Browserify script compressed', function(t) {
+    var app = { plugins: new broadway.App() };
+
+    app.plugins.use(new Browserify(), {
+        app: app,
+        compress: true
+    });
+
+    app.plugins.browserify(require.resolve('./test.js'), function(err, output) {
+
+        //console.log(output);
+        t.notOk(err, 'Should not be an error');
+        t.ok(output, 'Script should exist');
+        t.ok(/http:\/\/www.vast.com\/cars/.test(output), 'Output should contain static string from source script.');
+        t.notOk(/\s{2}/.test(output), 'Output should be compressed (e.g. never more than 1 whitespace side by side).');
+        t.notOk(/\/\*\* THIS IS A COMMENT FOR TESTING \*\*\//.test(output), 'Output should not have comments');
+        t.end();
+    });
 });
 
 
-test('Browserify script', function(t) {
 
-    var output = app.plugins.browserify(require.resolve('./test.js'));
+test('Browserify script uncompressed', function(t) {
+    var app = { plugins: new broadway.App() };
 
-    console.log(output);
-    t.ok(output, 'Script should exist');
+    app.plugins.use(new Browserify(), {
+        app: app,
+        compress: false
+    });
 
-    t.end();
+    app.plugins.browserify(require.resolve('./test.js'), function(err, output) {
+        
+        //console.log(output);
+        t.notOk(err, 'Should not be an error');
+        t.ok(output, 'Script should exist');
+        t.ok(/http:\/\/www.vast.com\/cars/.test(output), 'Output should contain static string from source script.');
+        t.ok(/\s{2}/.test(output), 'Output should be not compressed (e.g. more than 1 whitespace side by side).');
+        t.ok(/\/\*\* THIS IS A COMMENT FOR TESTING \*\*\//.test(output), 'Output should have comments');
+        t.end();
+    });
+});
+
+test('Browserify script with ignore', function(t) {
+    var app = { plugins: new broadway.App() };
+
+    app.plugins.use(new Browserify(), {
+        app: app,
+        compress: false,
+        ignore: ['/src/github/mixdown/plugins/node_modules/browserify/node_modules/browser-resolve/builtin/url.js']
+    });
+
+    app.plugins.browserify(require.resolve('./test.js'), function(err, output) {
+        
+        console.log(output);
+        t.notOk(err, 'Should not be an error');
+        t.end();
+    });
 });
--- tests/tap/test.js
+++ tests/tap/test.js
@@ -1,3 +1,4 @@
+/** THIS IS A COMMENT FOR TESTING **/
 var url = require('url'),
     uri = 'http://www.vast.com/cars';
 
